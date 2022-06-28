3b16a76c1e8ad01fd251fdd47cd9cbf8a50af56b
hellyanalves <hellyan.oliveira@quintoandar.com.br>
2022-06-28 15:27:57 -0300

    Added birth-date Component, using reducer to control state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap-country-select/dist/react-bootstrap-country-select.css';
 import './App.css';
-import React, { useState, useRef, forwardRef } from 'react';
+import React, { useState, useRef, forwardRef, useReducer } from 'react';
 
 const countries = [
   { title: 'Brasil', id: 1 },
@@ -44,6 +44,37 @@ const PhoneNumber = forwardRef(
   }
 )
 
+function Days(props) {
+  const days = Array.from({length: 30}, (_, i) => i + 1);
+  return (
+    <select class="col-sm-2  form-control" type="select" name="birth-day" id="birth-day" onChange={props.handleBirthDateChanged}>
+      {
+        days.map(day =>
+          <option key={day}>{day}</option>
+        )
+      }
+    </select>
+  );
+}
+
+function BirthDateEdit(props) {
+  
+  return (    
+    <div class="form-group row">
+      <label htmlFor="birth-year" class="col-form-label">Email</label>
+      <select class="col-sm-2  form-control" type="select" name="birth-year" id="birth-year" onChange={props.handleYearChanged} value={props.birthDate.getYear()}>
+        <option>1990</option>
+        <option>1991</option>
+      </select>
+      <select class="col-sm-2 form-control" type="select" name="birth-month" id="birth-month" onChange={props.handleMonthChanged}>
+        <option value='0'>Janeiro</option>
+        <option value='1'>Fevereiro</option>
+      </select>
+      <Days handleBirthDateChanged={props.handleDayChanged}/>
+    </div>
+  );
+}
+
 function SubmitFormButton() {
   return ( 
     <button type="submit"class="btn btn-primary">Register</button>
@@ -54,6 +85,8 @@ function Form(){
   const phoneRef = useRef(null);
   const countryRef = useRef(null);
   const [country, setCountry] = useState(undefined);
+  // const [birthDate, setBirthDate] = useState(new Date(1990,0,1));
+  const [birthDate, dispatch] = useReducer(birthDateReducer, new Date(1990,0,1));
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -72,12 +105,75 @@ function Form(){
     setCountry(e.target.value);
   }
 
+  /*
+  function handleBirthDateChanged(e){
+    console.log(birthDate);
+    console.log(e.target.id);
+    console.log(e.target.value);
+    switch(e.target.id){
+      case 'birth-year':
+        setBirthDate(new Date(e.target.value, birthDate.getMonth(), birthDate.getDay()));
+        break;
+      case 'birth-month':
+        setBirthDate(new Date(birthDate.getYear(), e.target.value, birthDate.getDay()));    
+        break;
+      case 'birth-day':
+          setBirthDate(new Date(birthDate.getYear(), birthDate.getMonth(), e.target.value));    
+          break;
+      default:
+        setBirthDate(birthDate);
+    }
+  }
+  */
+
+  function handleYearChanged(e){
+    dispatch({
+        type: 'year_changed',
+        year: e.target.value
+      })
+  }
+  
+  function handleMonthChanged(e){
+    dispatch({
+        type: 'month_changed',
+        month: e.target.value
+      })
+  }
+
+  function handleDayChanged(e){
+    dispatch({
+        type: 'day_changed',
+        day: e.target.value
+      })
+  }
+
+  function birthDateReducer(birthDate, action){
+    switch(action.type){
+      case 'year_changed':
+        return(new Date(action.year, birthDate.getMonth(), birthDate.getDay()));
+      case 'month_changed':
+        return(
+          new Date(birthDate.getYear(), action.month, birthDate.getDay())
+        );
+      case 'day_changed':
+        return(
+          new Date(birthDate.getYear(), birthDate.getMonth(), action.day)
+        ); 
+      default:
+        return(
+          birthDate
+          );
+    }
+  }
+
+  console.log("Form ", birthDate);  
   return (
     <h1>Register into my awesome system</h1>,
     <form onSubmit={handleSubmit}>
       <CountrySelect handleSelect={handleCountryChangedState} ref={countryRef} countryCode={country}/>
       <PhoneNumber ref={phoneRef} countryCode={country}/>
       <EmailTextEdit/>
+      <BirthDateEdit handleYearChanged={handleYearChanged} handleMonthChanged={handleMonthChanged} handleDayChanged={handleDayChanged} birthDate={birthDate}/>
       <SubmitFormButton/>
     </form>
   )
